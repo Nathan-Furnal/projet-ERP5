884c27f8675e3b0fd8b604877683e1c0969d4008
Nathan Furnal <nathanfurnal.dev@gmail.com>
2022-12-18 17:18:44 +0100

    add buyer model

---
+++ odoo-app/realtor/models/realtor_buyer_model.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+class Buyer(models.Model):
+    _inherit = 'res.partner'
+    _description = 'Extension of res.partner'
+
+    offer_id = fields.One2many('realtor.offer', 'buyer_id', string="Offer")
