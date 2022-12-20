920647092d25911ae92ea64a2fb616c45850b03b
Nathan Furnal <nathanfurnal.dev@gmail.com>
2022-12-20 01:46:45 +0100

    update manifest

--- odoo-app/realtor/__manifest__.py
+++ odoo-app/realtor/__manifest__.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "/mnt/extra-addons/realtor",
+    'name': "Realtor",
 
     'summary': """
-    A realtor application.
-       """,
+        A real estate management app.""",
 
     'description': """
-        An application module for real estate management.
+        A real estate management app with interaction between apartments and buyers.
     """,
 
     'author': "Nathan Furnal",
-    'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -25,12 +23,15 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/realtor_apartment_views.xml',
+        'views/realtor_offer_views.xml',
+        'views/res_users_views.xml',
+        'views/realtor_menus.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    
     'application': True,
 }
