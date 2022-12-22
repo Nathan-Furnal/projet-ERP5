12f85ae7f86d7057e3da5c39ddcf1522e8952a8b
Nathan Furnal <nathanfurnal.dev@gmail.com>
2022-12-22 14:30:33 +0100

    add home page

--- realtorclient/realtorclient/settings.py
+++ realtorclient/realtorclient/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # My own apps
+    "odoo_connect.apps.OdooConnectConfig",
+    "odoo_realtor.apps.OdooRealtorConfig",
+    "home.apps.HomeConfig",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = "realtorclient.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- realtorclient/realtorclient/urls.py
+++ realtorclient/realtorclient/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('connect/', include('odoo_connect.urls')),
+    path('apartments/', include('odoo_realtor.urls')),
+    path('', include('home.urls')),
 ]
