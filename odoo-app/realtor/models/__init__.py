84e1fb28f5a3874d003c5fe119cb4fd0c5af89f9
Nathan Furnal <nathanfurnal.dev@gmail.com>
2022-12-22 19:27:50 +0100

    clean cached

--- odoo-app/realtor/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import realtor_apartment
-from . import realtor_offer
-from . import res_users
-from . import realtor_product
--- realtorclient/odoo_realtor/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OdooRealtorConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "odoo_realtor"
